a7d8968a07023b7e905a3f406d5dd7336be09ede
James Cox <j-cox03@hotmail.com>
2017-09-25 20:22:33 +0100

    Added name to registrations and jwt

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < ApplicationController
     if(User.exists?(email: params[:email]))
       render json: {respons: "user exists"}
     else
-      user = User.create({email: params[:email], password: params[:password]})
+      user = User.create({first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password]})
       render json: token_hash(user)
     end
   end
