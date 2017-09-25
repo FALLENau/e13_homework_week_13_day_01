95fa9cbb6e49ff2a7420f41ceebd0dab7282cf33
James Cox <j-cox03@hotmail.com>
2017-09-25 16:45:43 +0100

    added first name and last name to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922112008) do
+ActiveRecord::Schema.define(version: 20170925154408) do
 
   create_table "accounts", force: :cascade do |t|
     t.text "name"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20170922112008) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
