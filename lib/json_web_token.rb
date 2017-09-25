6ccec73d3fe7ffb793ad9bb568b48dd49cdf8092
James Cox <j-cox03@hotmail.com>
2017-09-25 16:15:38 +0100

    Fixed web token encoding and decoding

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,11 +1,13 @@
 class JsonWebToken
   def self.encode(payload)
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, Rails.application.secrets.secret_key_base, "HS256")
   end
 
   def self.decode(token)
-    return HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.secrets.secret_key_base)[0])
-  rescue
-    nil
+    return HashWithIndifferentAccess.new(
+      JWT.decode(token, Rails.application.secrets.secret_key_base, true, {algorithm: "HS256"})[0]
+    )
+  rescue => error
+    puts error
   end
 end
