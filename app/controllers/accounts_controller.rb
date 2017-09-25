d1f3f4da01a9d5de68baa745a4d871daff401b58
James Cox <j-cox03@hotmail.com>
2017-09-25 14:15:27 +0100

    added auth check to accounts controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,5 @@
 class AccountsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_request!
 
     def index
      @accounts = current_user.accounts
