0f1f2ec43f3cacbd448ff8aee1dc6baa95150517
James Cox <j-cox03@hotmail.com>
2017-09-25 12:54:26 +0100

    added payload method to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,27 @@ class SessionsController < ApplicationController
   respond_to :json
 
   def create
-    user = User.where(email: params[:email]).first
-
-    if user&.valid_password?(params[:password])
-      render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
-    else###TODO: remove [:id] from 6 when api to react works.
-      head(:unauthorized)
+    user = User.find_for_database_authentication(email: params[:email])
+    if user.valid_password?(params[:password])
+      render json: payload(user)
+    else
+      render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
     end
   end
 
   def destroy
 
+  end
+
+  private
+  def payload(user)
+    if(user && user.id)
+      return {
+        auth_token: JsonWebToken.encode(user_id: user.id),
+        user: {id: user.id, email: user.email}
+      }
+    else
+      return nil
+    end
   end
 end
