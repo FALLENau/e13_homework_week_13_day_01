cdae31ce06488098d5f76682c6d395e3bf72ea6e
eoghanscrowley <eoghanscrowley@gmail.com>
2017-09-26 12:02:37 +0100

    fix typo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < ApplicationController
 
   def create
     if(User.exists?(email: params[:email]))
-      render json: {respons: "user exists"}
+      render json: {response: "user exists"}
     else
       user = User.create({first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password]})
       render json: token_hash(user)
